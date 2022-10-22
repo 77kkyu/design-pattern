2ecaa23c4a5d414576739bece8aa474b0cf8ad83
77kkyu <77kkyu@gmail.com>
2022-10-22 23:10:05 +0900

    feat EventService

---
+++ proxy-pattern/src/main/java/com/example/proxypattern/ex1/service/EventService.java
@@ -0,0 +1,6 @@
+package com.example.proxypattern.ex1.service;
+
+public interface EventService {
+	void createEvent();
+	void publishEvent();
+}
---
+++ proxy-pattern/src/main/java/com/example/proxypattern/ex2/domain/Book.java
@@ -0,0 +1,12 @@
+package com.example.proxypattern.ex2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+	private String title;
+}
