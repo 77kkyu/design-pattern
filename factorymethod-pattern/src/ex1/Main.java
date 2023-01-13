26e5a20490a263aa5747e0701b16474cd78ba2a1
77kkyu <77kkyu@gmail.com>
2023-01-14 00:39:41 +0900

    feat Main

---
+++ factorymethod-pattern/src/ex1/Main.java
@@ -0,0 +1,19 @@
+package ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        shape1.draw();
+
+        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+        shape2.draw();
+
+        Shape shape3 = shapeFactory.getShape("SQUARE");
+        shape3.draw();
+
+        Shape shape4 = shapeFactory.getShape("TEST");
+        shape4.draw();
+    }
+}
