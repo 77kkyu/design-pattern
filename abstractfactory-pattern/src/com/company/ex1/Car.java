401b6ed90cff2f859d42ee1890dbe444b2a7cfef
77kkyu <77kkyu@gmail.com>
2023-01-31 00:03:12 +0900

    feat Car

---
+++ abstractfactory-pattern/src/com/company/ex1/Car.java
@@ -0,0 +1,19 @@
+package com.company.ex1;
+
+public class Car {
+	private Frame frame;
+	private Wheel wheel;
+
+	public Car(Frame frame, Wheel wheel) {
+		this.frame = frame;
+		this.wheel = wheel;
+	}
+
+	public Frame getFrame() {
+		return frame;
+	}
+
+	public Wheel getWheel() {
+		return wheel;
+	}
+}
