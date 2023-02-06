72a0691dc286e74df59b9273f8d2de9ae5a6bd47
77kkyu <77kkyu@gmail.com>
2023-02-07 00:45:13 +0900

    feat KoreaTireProduct

---
+++ abstractfactory-pattern/src/com/company/ex2/KoreaTireProduct.java
@@ -0,0 +1,10 @@
+package com.company.ex2;
+
+public class KoreaTireProduct extends TireProduct {
+	public KoreaTireProduct() {}
+
+	@Override
+	public String makeAssemble() {
+		return "국산 타이어";
+	}
+}
