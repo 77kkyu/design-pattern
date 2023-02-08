2ea1032acd7e55ca50769e72fa39b923f2936afe
77kkyu <77kkyu@gmail.com>
2023-02-09 00:00:29 +0900

    feat StateTireProduct

---
+++ abstractfactory-pattern/src/com/company/ex2/StateTireProduct.java
@@ -0,0 +1,11 @@
+package com.company.ex2;
+
+public class StateTireProduct extends TireProduct {
+	public StateTireProduct() {
+	}
+
+	@Override
+	public String makeAssemble() {
+		return "미국산 타이어";
+	}
+}
